d3ddff31c737c9f94d6e9d2f2bd80d0b7d8d2cca
fernando <fdabrego@gmail.com>
2022-07-11 20:18:31 -0300

    Cambios ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCount from "./ItemCount";
 import styles from './ItemDetail.module.css';
 
 export default function ItemDetail ({img, nombre, precio, detalle}) {
+    const [number, setNumber] = useState(0);
+    const onAdd = (cantidad) =>{
+        setNumber(cantidad);
+    };
 return(
     <>
         <div className={styles.contenedorDetalle}>
@@ -10,7 +14,7 @@ return(
             <h2>{nombre}</h2>
             <h3>{precio}</h3>
             <h4>{detalle}</h4>
-            <ItemCount initial={1} stock={8} />
+            <ItemCount initial={1} stock={8} onAdd={onAdd} />
         </div>
     </>
 );
